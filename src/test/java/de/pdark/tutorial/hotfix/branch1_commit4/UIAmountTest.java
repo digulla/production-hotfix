fe0d8ab997da8b3591dd3dfef8437b48ba52a4c1
Aaron Digulla <digulla@hepe.com>
2024-09-09 20:49:28 +0200

    Unused imports

--- src/test/java/de/pdark/tutorial/hotfix/branch1_commit4/UIAmountTest.java
+++ src/test/java/de/pdark/tutorial/hotfix/branch1_commit4/UIAmountTest.java
@@ -4,8 +4,6 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-import de.pdark.tutorial.hotfix.branch1_commit4.UIAmount;
-
 class UIAmountTest {
 
     @Test
